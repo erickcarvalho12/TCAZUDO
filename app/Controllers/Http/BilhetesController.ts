d2764f2c93d081221fe01f36f514c6df81310f45
mikael1909 <mikael_1909@hotmail.com>
2021-06-09 23:57:09 -0300

    amanha tem mais

--- app/Controllers/Http/BilhetesController.ts
+++ app/Controllers/Http/BilhetesController.ts
@@ -1,12 +1,21 @@
  import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Rifa from 'App/Models/Rifa'
+import Tipo from 'App/Models/Tipo'
 
 export default class BilhetesController {
 
 
-    public async store({ request, response, params, auth,view }: HttpContextContract) {
+    public async store({ params,view }: HttpContextContract) {  
+    const rifa = await Rifa.find(params.rifa_id)
 
-      
-    console.log('( ' ,params.rifa_id,') ' )
+
+    const qtdBilhetes = await Tipo.query()
+        .where('id', rifa!!.tipoId ).firstOrFail()
+    
+    for (let a = 0; a < qtdBilhetes.quantidadeBilhetes; a++) {
+        
+    }
+    
     return view.render('home/index')
   }
 }
